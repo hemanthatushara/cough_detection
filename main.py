f2dd5ee4aec86f9b20016c7218d3f5be9742e465
hemanthatushara <50065348+hemanthatushara@users.noreply.github.com>
2021-01-29 14:50:24 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+import tensorflow_io as tfio
+from IPython.display import Audio
+
+
+audio = tfio.audio.AudioIOTensor('/content/pos-0421-084-cough-m-50-0.mp3')
+
+print(audio)
+https://github.com/hemanthatushara/cough_detection.git
